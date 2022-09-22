d5e9d0b99adf62ede7dd0727bfaa0a6efbd21369
Mayank Chandak <chandakmayank@gmail.com>
2022-09-22 22:04:44 +0300

    table validator module

---
+++ app/validators/tables.validator.js
@@ -0,0 +1,9 @@
+//* validators/register.validator.js
+const Joi = require('joi');
+
+const tableSchema = Joi.object({
+    table_no: Joi.number().greater(0).required(),
+    capacity: Joi.number().greater(0).required(),
+});
+
+module.exports = tableSchema;
