e307bbc43f847b390046bdd2612a611928b429f9
Mayank Chandak <chandakmayank@gmail.com>
2022-09-23 04:20:25 +0300

    reservation validator

--- app/validators/index.js
+++ app/validators/index.js
@@ -1,9 +1,9 @@
 //* validators/index.js
 const register = require('./register.validator')
 const tables = require('./tables.validator')
-const reservation = require('./reservation.validator')
+const {reservation,reservationRemove,reserveTable} = require('./reservation.validator')
 
 
 module.exports = {
-    register, tables, reservation
+    register, tables, reservation,reservationRemove, reserveTable
 }
--- app/validators/reservation.validator.js
+++ app/validators/reservation.validator.js
@@ -3,7 +3,6 @@
 const Joi = require('joi');
 
 const reservationSchema = Joi.object({
-    reservation_id: Joi.number().greater(0).required(),
     table_no: Joi.number().greater(0).required(),
     party_size: Joi.number().required(),
     reservation_date: Joi.date().required(),
@@ -12,4 +11,27 @@ const reservationSchema = Joi.object({
     username: Joi.string().required()
 });
 
-module.exports = reservationSchema;
+const slotChecker = Joi.object({
+    party_size: Joi.number().greater(0).required(),
+    reservation_date: Joi.date().required(),
+    start_time: Joi.date().timestamp().required(),
+    end_time: Joi.date().timestamp().required()
+})
+
+const reservationRemove = Joi.object({
+    reservation_id: Joi.string().guid().required(),
+})
+
+const reserveTable = Joi.object({
+    party_size: Joi.number().greater(0).required(),
+    reservation_date: Joi.date().greater('now').required(),
+    start_time: Joi.number().min(1200).max(2358).required(),
+    duration: Joi.number().min(10).max(720).required()
+})
+
+// module.exports = reservationSchema, slotChecker, reservationRemove, reserveTable;
+
+module.exports = {
+    reservationRemove: reservationRemove,
+    reserveTable: reserveTable
+};
