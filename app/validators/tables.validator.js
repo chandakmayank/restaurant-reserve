fca39739d7bf52bc1404cd8ee325406c3e8fc5dd
Mayank Chandak <chandakmayank@gmail.com>
2022-09-23 04:20:43 +0300

    validate table capacity

--- app/validators/tables.validator.js
+++ app/validators/tables.validator.js
@@ -3,7 +3,7 @@ const Joi = require('joi');
 
 const tableSchema = Joi.object({
     table_no: Joi.number().greater(0).required(),
-    capacity: Joi.number().greater(0).required(),
+    capacity: Joi.number().greater(0).less(13).required(),
 });
 
 module.exports = tableSchema;
