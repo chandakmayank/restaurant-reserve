8e52ac66a2fe7278356bdd4c8ddf8c1dfa866d93
Mayank Chandak <chandakmayank@gmail.com>
2022-09-24 12:51:02 +0300

    registration validator checks if role provided

--- app/validators/register.validator.js
+++ app/validators/register.validator.js
@@ -4,7 +4,8 @@ const Joi = require('joi');
 const registerSchema = Joi.object({
     email: Joi.string().email().lowercase().required(),
     username: Joi.number().greater(1000).less(9999).required(),
-    password: Joi.string().min(6).required()
+    password: Joi.string().min(6).required(),
+    roles: Joi.array().items(Joi.string())
 });
 
 module.exports = registerSchema;
